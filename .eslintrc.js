2be841932c1154624b41819239509145f4b7ba0e
lbrossault <brossault.leo@gmail.com>
2023-07-31 10:16:54 +0200

    ci: test files

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false
+  },
+  extends: ["@nuxtjs", "plugin:nuxt/recommended", "prettier"],
+  plugins: ["only-warn"],
+  ignorePatterns: ["tests/"],
+  // add your custom rules here
+  rules: {
+    "vue/no-v-html": "off"
+  },
+  globals: {
+    $nuxt: true,
+    datalayer: true,
+    jwplayer: true,
+    zE: true
+  }
+}
---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ["stylelint-scss", "stylelint-order"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-prettier",
+    "stylelint-config-recommended-scss"
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {}
+}
