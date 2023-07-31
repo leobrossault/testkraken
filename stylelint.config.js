2f2cd5be3a1be6a90b0e896efbfdaf3f5319ae1a
lbrossault <brossault.leo@gmail.com>
2023-07-31 12:48:07 +0200

    ci: test files

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: ["stylelint-order"],
+  plugins: ["stylelint-scss", "stylelint-order"],
   extends: ["stylelint-config-standard", "stylelint-config-prettier"],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
